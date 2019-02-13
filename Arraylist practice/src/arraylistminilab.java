517aaa454f2ef4fd7d315751b150acd5b9e53a81
CcarrieZeng <han.zeng@stjlabs.com>
2019-02-13 11:23:01 -0500

    finished arraylistminilab but the second method has some errors

--- Arraylist practice/src/arraylistminilab.java
+++ Arraylist practice/src/arraylistminilab.java
@@ -1,9 +1,44 @@
+import java.util.ArrayList;
 
 public class arraylistminilab {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Integer>testing=new ArrayList<Integer>();
+		testing.add(2);
+		testing.add(6);
+		testing.add(8);
+		testing.add(50);
+		System.out.println(getListOfFactors(762));
+		keepOnlyCompositeNumbers(testing);
 
 	}
+	 public static ArrayList<Integer> getListOfFactors(int number)
+	  {  
+		 ArrayList<Integer>factor=new ArrayList<Integer>();
+		 for(int i=1;i<=number;i++) {
+			if(number%i==0) {
+				factor.add(i);
+			} 
+			factor.remove(Integer.valueOf(1));
+			factor.remove(Integer.valueOf(number));
+		 }
+		 
+		 return factor;
+	  }
+	 
+	  public static void keepOnlyCompositeNumbers( ArrayList<Integer> nums )
+	  {
+		  ArrayList<Integer>composite=new ArrayList<Integer>();
+		  for(int i=0;i<composite.size();i++) {
+			  int num=composite.get(i);
+			  if(getListOfFactors(num).size()>2) {
+				  composite.add(num); 
+			  }
+				 
+		  }
+		 System.out.println(composite);
+		  }
+	  }
 
-}
+
